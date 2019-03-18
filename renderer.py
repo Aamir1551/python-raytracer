fab743ae220553044432af9162ab2767b6d677f2
aamir soni <ll17a26s@leeds.ac.uk>
2019-03-18 23:00:43 +0000

    second Sphere added

--- renderer.py
+++ renderer.py
@@ -5,7 +5,8 @@ from canvas import *
 BACKGROUND_COLOUR = "255   0   0 "
 
 
-hittables = [Sphere(np.array([0, -1, 3]), 0.9, "255 255   0 ")]
+hittables = [Sphere(np.array([0, -1, 3]), 1, "255 255   0 "),
+             Sphere(np.array([2, 0, 4]), 1, "0   0   255 ")]
 
 cscene = Scene(hittables, "0   0   0  ")
 
